7add709b769cf1b85f545c94dd6cf06dbf88290e
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-10-31 20:31:46 +0700

    make product migration

---
+++ database/migrations/2020_10_31_132839_product.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Product extends Migration
+{
+    var $table = 'tbl_produk';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->increments('id_product');
+            $table->char('name', 100);
+            $table->double('price');
+            $table->integer('stock');
+            $table->text('summary')->nullable();
+            $table->text('description')->nullable();
+            $table->text('image_product')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+}
